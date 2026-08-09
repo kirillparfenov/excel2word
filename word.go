cd2af1e834ba173553fed3d796de71d3c080b26b
kirillparfenov <parfenov1991@mail.ru>
2026-08-09 21:57:10 +0300

    структура document

--- word.go
+++ word.go
@@ -7,6 +7,18 @@ import (
 	"github.com/lukasjarosch/go-docx"
 )
 
+// Document
+// outDir - директория сохранения файла
+// rowIndex - индекс реплейсмента
+// wordPath - путь до word файла-шаблона
+// replacements - реплейсменты для word файла
+type Document struct {
+	outDir       string
+	rowIndex     int
+	wordPath     string
+	replacements docx.PlaceholderMap
+}
+
 func replaceWordReplacements(dir string, excelReplacements []docx.PlaceholderMap) ([]string, error) {
 	wordPath, err := findFileByExt(dir, ".docx")
 	if err != nil {
@@ -29,33 +41,40 @@ func replaceWordReplacements(dir string, excelReplacements []docx.PlaceholderMap
 
 	createdDocs := make([]string, 0, len(excelReplacements)+1)
 	for i, replacements := range excelReplacements {
-		outName := fmt.Sprintf("document_%03d.docx", i+1)
-		outPath := filepath.Join(outDir, outName)
-
-		if err := generateDocx(wordPath, outPath, replacements); err != nil {
-			return nil, fmt.Errorf("ошибка в процессе генерации .docx: %w", err)
+		docName, err := createDoc(&Document{
+			outDir:       outDir,
+			rowIndex:     i,
+			wordPath:     wordPath,
+			replacements: replacements,
+		})
+		if err != nil {
+			return nil, err
 		}
-		createdDocs = append(createdDocs, outName)
+		createdDocs = append(createdDocs, docName)
 	}
 
 	return createdDocs, nil
 }
 
-// generateDocx открывает шаблон заново (т.к. замена мутирует документ в памяти),
+// createDoc создает документ
+// открывает шаблон заново (т.к. замена мутирует документ в памяти),
 // подставляет значения вместо {ключ} и сохраняет результат.
-func generateDocx(templatePath, outPath string, replacements docx.PlaceholderMap) error {
-	doc, err := docx.Open(templatePath)
+func createDoc(doc *Document) (string, error) {
+	word, err := docx.Open(doc.wordPath)
 	if err != nil {
-		return fmt.Errorf("не удалось открыть шаблон: %w", err)
+		return "", fmt.Errorf("не удалось открыть шаблон word: %w", err)
 	}
-	defer doc.Close()
+	defer word.Close()
 
-	if err := doc.ReplaceAll(replacements); err != nil {
-		return fmt.Errorf("не удалось выполнить замену: %w", err)
+	if err := word.ReplaceAll(doc.replacements); err != nil {
+		return "", fmt.Errorf("не удалось выполнить замену: %w", err)
 	}
 
-	if err := doc.WriteToFile(outPath); err != nil {
-		return fmt.Errorf("не удалось сохранить файл: %w", err)
+	outName := fmt.Sprintf("document_%03d.docx", doc.rowIndex+1)
+	outPath := filepath.Join(doc.outDir, outName)
+	if err := word.WriteToFile(outPath); err != nil {
+		return "", fmt.Errorf("не удалось сохранить файл: %w", err)
 	}
-	return nil
+
+	return outName, nil
 }
